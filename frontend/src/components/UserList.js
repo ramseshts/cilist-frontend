35c4c4a9a50bd74d715c36a3adac597c817f2d03
Ramses S Hts <ramseshutasoit77@gmail.com>
2023-02-17 17:38:37 +0700

    Update UserList.js

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -33,7 +33,7 @@ const UserList = () => {
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
-        <Banner title="Users List Production" />
+        <Banner title="Users List Prod" />
         <Link to={`add`} className="button is-info">
           Add New
         </Link>
